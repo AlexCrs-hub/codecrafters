e9f12845c8bf5c8386a625afcdfd4269a0423e5f
AlexCrs-hub <56338148+AlexCrs-hub@users.noreply.github.com>
2023-07-04 16:49:25 +0300

    test

---
+++ Logic/DTO/UserDTO.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Logic.DTO
+{
+    public class UserDTO
+    {   
+        public Guid Id { get; set; }
+        public string Username { get; set; }
+        public string Password { get; set; }
+    }
+}
